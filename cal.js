02991214f659ad49b784dd8e1df19bbaa87c72c7
Martin Stone <martin@pagerduty.com>
2017-01-10 10:47:26 -0500

    initial commit

---
+++ cal.js
@@ -0,0 +1,36 @@
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? null : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function main() {
+	var url = getParameterByName("iCalURL");
+	
+	$.ajax({
+		url: url,
+		type: "text",
+		method: "get",
+		success: function(data) {
+			var jcalData = ICAL.parse(data);
+			var comp = new ICAL.Component(jcalData);
+			var vevents = comp.getAllSubcomponents("vevent");
+			var events = [];
+			vevents.forEach(function(vevent) {
+				var event = new ICAL.Event(vevent);
+				events.push({
+					title: event.summary,
+					start: (new ICAL.Time(event.startDate)).toString(),
+					end: (new ICAL.Time(event.endDate)).toString(),
+				});
+			});
+			$('#calendar').fullCalendar({
+				events: events
+			});
+		}
+	})
+}
+
+
+$(document).ready(main);
