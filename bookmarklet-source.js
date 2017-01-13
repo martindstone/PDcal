3018bb11140fec831253f681de7e2af2fd8de44b
Martin Stone <martin@pagerduty.com>
2017-01-13 14:55:33 -0500

    bookmarklet

---
+++ bookmarklet-source.js
@@ -0,0 +1,14 @@
+javascript: (
+	function() {
+		$('ul.cal-options-menu').each(function() {
+			var div = $(this).parent().parent();
+			var ul = $(this);
+			$(this).find('li.just-my-cal-option > a').each(function() {
+				var icalurl = $(this).attr('href').replace(/\?.*$/, '');
+				var href = 'https://martindstone.github.io/PDcal/index.html?iCalURL=' + icalurl;
+				$(ul).append('<li class="cal-subheader"><h4 class="dropdown-menu-subheader"><i class="icon icon-external-link-sign icon-fixed-width"></i>Public View</h4></li>');
+				$(ul).append('<li class="cal-option"><a class=action-link cal-link" href="' + href + '" target="_blank">Open in new tab</a>');
+			});
+		});
+	}
+)
