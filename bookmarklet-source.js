b8da9f0eb30becdc8c6613fc7a221ff530946ea1
Martin Stone <martin@pagerduty.com>
2017-02-09 15:53:31 -0800

    don't make more than one entry in the export menu when clicked repeatedly

--- bookmarklet-source.js
+++ bookmarklet-source.js
@@ -3,6 +3,9 @@ javascript: (
 		$('ul.cal-options-menu').each(function() {
 			var ul = $(this);
 			$(this).find('li.just-my-cal-option > a').each(function() {
+				if ( ul.find('i.icon-external-link-sign').length > 0 ) {
+					return;
+				}
 				var icalurl = $(this).attr('href').replace(/\?.*$/, '');
 				var href = 'https://martindstone.github.io/PDcal/index.html?iCalURL=' + icalurl;
 				$(ul).append('<li class="cal-subheader"><h4 class="dropdown-menu-subheader"><i class="icon icon-external-link-sign icon-fixed-width"></i>Public View</h4></li>');
